75582bd92a29909bc8481fabe7ef41219a58711c
Aliaksei Mastounikau <aliaksei.mastounikau@easypark.net>
2024-08-10 18:10:45 +0200

    Lecture 4. Result

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -1,6 +1,33 @@
 import SwiftUI
 
-class EmojiMemoryGame {
-    var model: MemoryGame<String>
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "👽", "☠️", "🍭"]
+
+    @Published private var model = createMemoryGame()
+
+    static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 16) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+
+    // MARK: - Intents
+
+    func shuffle() {
+        model.shuffle()
+    }
+
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
 }
 
