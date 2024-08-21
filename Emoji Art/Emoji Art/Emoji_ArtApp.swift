4ca596e09d05c35da6d543097a8fa86c91573eff
MALX <mostovnikov.alex@gmail.com>
2024-08-21 19:37:49 +0200

    Lecture 11. Result

--- Emoji Art/Emoji Art/Emoji_ArtApp.swift
+++ Emoji Art/Emoji Art/Emoji_ArtApp.swift
@@ -3,10 +3,12 @@ import SwiftUI
 @main
 struct Emoji_ArtApp: App {
     @StateObject var defaultDocument = EmojiArtDocument()
+    @StateObject var paletteStore = PaletteStore(named: "Main")
 
     var body: some Scene {
         WindowGroup {
             EmojiArtDocumentView(document: defaultDocument)
+                .environmentObject(paletteStore)
         }
     }
 }
