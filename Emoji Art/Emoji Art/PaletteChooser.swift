44b4b1f5defe183cc86a69bb7fb928790b96f757
MALX <mostovnikov.alex@gmail.com>
2024-08-23 18:40:05 +0200

    Lecture 13. Result

--- Emoji Art/Emoji Art/PaletteChooser.swift
+++ Emoji Art/Emoji Art/PaletteChooser.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct PaletteChooser: View {
     @EnvironmentObject var store: PaletteStore
+    @State private var showPaletteEditor = false
+    @State private var showPalletList = false
 
     
     var body: some View {
@@ -10,6 +12,16 @@ struct PaletteChooser: View {
             view(for: store.palettes[store.cursorIndex])
         }
         .clipped()
+        .sheet(isPresented: $showPaletteEditor) {
+            PaletteEditor(palette: $store.palettes[store.cursorIndex])
+                .font(nil)
+        }
+        .sheet(isPresented: $showPalletList) {
+            NavigationStack {
+                EditablePaletteList(store: store)
+                    .font(nil)
+            }
+        }
     }
 
     var chooser: some View {
@@ -17,12 +29,34 @@ struct PaletteChooser: View {
             store.cursorIndex += 1
         }
         .contextMenu {
+            gotoMenu
             AnimatedActionButton("New", systemImage: "plus") {
-                store.insert(name: "Math", emojis: "✖️➗🟰")
+                store.insert(name: "", emojis: "")
+                showPaletteEditor = true
             }
             AnimatedActionButton("Delete", systemImage: "minus.circle", role: .destructive) {
                 store.palettes.remove(at: store.cursorIndex)
             }
+            AnimatedActionButton("Edit", systemImage: "pencil") {
+                showPaletteEditor = true
+            }
+            AnimatedActionButton("List", systemImage: "list.bullet.rectangle.portrait") {
+                showPalletList = true
+            }
+        }
+    }
+
+    private var gotoMenu: some View {
+        Menu {
+            ForEach(store.palettes) { palette in
+                AnimatedActionButton(palette.name) {
+                    if let index = store.palettes.firstIndex(where: { $0.id == palette.id }) {
+                        store.cursorIndex = index
+                    }
+                }
+            }
+        } label: {
+            Label("Go To", systemImage: "text.insert")
         }
     }
 
